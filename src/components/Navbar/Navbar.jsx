172f7b7dd207ef2fd2acd4617e43893d5b60dd14
unknown <luizhmatoso1340@gmail.com>
2025-03-21 11:51:21 -0300

    Add Navbar responsivess & styles

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 import styles from "./Navbar.module.css";
 
-import logo from "../../assets/logo.png";
+import logo from "../../assets/logo2.png";
+import menuIcon from "../../assets/menuIcon.png";
+import closeIcon from "../../assets/closeIcon.png";
+
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav className={styles.navbar}>
       <img className={styles.logo} src={logo} alt="" />
       <div className={styles.menu}>
-        <ul className={styles.menuItems}>
+        <img
+          className={styles.menuBtn}
+          src={menuOpen ? menuIcon : closeIcon}
+          alt="Menu button"
+          onClick={() => setMenuOpen(!menuOpen)}
+        />
+        <ul
+          className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
+          onClick={() => setMenuOpen(false)}
+        >
           <li>
             <a href="/">Home</a>
           </li>
