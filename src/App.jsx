3a80828fa8b5e1fd7da94a8c857d5783f39ece7d
unknown <luizhmatoso1340@gmail.com>
2025-03-22 11:46:44 -0300

    Add About section (missing about image)

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import styles from "./App.module.css";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
+import About from "./components/About/About";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
                 <>
                   <Navbar />
                   <Hero />
+                  <About />
                 </>
               }
             />
---
+++ src/components/About/About.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+import styles from "./About.module.css";
+
+import lightIcon from "../../assets/lightIcon.png";
+import sparklesIcon from "../../assets/sparklesIcon.png";
+import graphIcon from "../../assets/graphIcon.png";
+
+const About = () => {
+  return (
+    <section className={styles.container} id="about">
+      <h2 className={styles.title}>About</h2>
+      <div className={styles.content}>
+        <img className={styles.aboutImage} src="" alt="About icon" />
+        <ul className={styles.aboutItems}>
+          <li className={styles.aboutItem}>
+            <img src={lightIcon} alt="" />
+            <div>
+              <h3>Fast Link Shortening</h3>
+              <p>Shorten your links in an instant!</p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={sparklesIcon} alt="" />
+            <div>
+              <h3>Secure Links</h3>
+              <p>Keep your links safe and private.</p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={graphIcon} alt="" />
+            <div>
+              <h3>Track Your Links</h3>
+              <p>Get insights and analytics on your links’ performance.</p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+};
+
+export default About;
